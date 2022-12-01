d23cf92e789e6c57f433d17fa6544430ae81bed0
AlpezDev <alpezdev@gmail.com>
2022-11-30 23:52:51 -0500

    Splash and login

---
+++ lightControl/src/lightcontrol/LightControl.java
@@ -0,0 +1,27 @@
+package lightcontrol;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LightControl {
+
+    public static void main(String[] args) {
+        Runnable run = () -> {
+            Splash loading = new Splash();
+            loading.setVisible(true);
+            
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(LightControl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            loading.dispose();
+            Login login = new Login();
+            login.setVisible(true);            
+        };
+        Thread hiloSplash = new Thread(run);
+        hiloSplash.start();
+    }
+    
+}
